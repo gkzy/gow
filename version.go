638700ee2fe4835f3d7dcf2c0a6e36e61fcb9e34
Sam <301109640@qq.com>
2020-10-16 13:52:23 +0800

    add mux router

--- version.go
+++ version.go
@@ -1,9 +1,8 @@
 package gow
 
 const (
+	// gow version
 	version = "v0.1.3"
-	pkg     = "https://github.com/gkzy/gow"
-	site    = "https://gow.22v.net"
 	logo    = `   ____   ______  _  __
   / ___\ /  _ \ \/ \/ /
  / /_/  >  <_> )     / 
