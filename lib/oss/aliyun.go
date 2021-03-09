8ab6d2dfbced8a1048acd3a1c4b2d6cad9d6fb3f
Sam <301109640@qq.com>
2021-03-09 09:42:22 +0800

    update version to v0.5.1

--- lib/oss/aliyun.go
+++ lib/oss/aliyun.go
@@ -1,6 +1,7 @@
 package oss
 
 import (
+	"errors"
 	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/gkzy/gow/lib/util"
@@ -33,8 +34,40 @@ func NewAliClient(accessKeyId, secret, endPoint, bucketName, serverUrl string) *
 	}
 }
 
-// Upload 上传文件
+// UploadFile 上传一个文件，返回远程地址及错误
+//	不强制重命名文件名
+//	like:   /dir/20210309/filename
+func (c *AliClient) UploadFile(reader io.Reader, dir, filename string) (url string, err error) {
+	if filename == "" {
+		err = errors.New("请传入上传后的文件名")
+		return
+	}
+	if dir == "" {
+		err = errors.New("请传入上传后的目录")
+	}
+	client, err := oss.New(c.EndPoint, c.AccessKeyId, c.Secret)
+	if err != nil {
+		err = fmt.Errorf("[client]init失败:%v", err)
+		return
+	}
+	bucket, err := client.Bucket(c.BucketName)
+	if err != nil {
+		return
+	}
+
+	// like /topic/20210309/filename
+	filePath := fmt.Sprintf("%s/%s/%s", dir, time.Now().Format("20060102"), filename)
+	err = bucket.PutObject(filePath, reader)
+	if err != nil {
+		return
+	}
+	url = fmt.Sprintf("%s%s", c.ServerUrl, filePath)
+	return
+}
+
+// Upload 上传文件	返回远程地址及错误
 //		url,err:=client.Upload(reader,dir,ext)
+//		会强制重命名文件名
 func (c *AliClient) Upload(reader io.Reader, dir, ext string) (url string, err error) {
 	if ext == "" {
 		ext = ".jpg"
