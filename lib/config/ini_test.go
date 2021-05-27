e8a436714d5803c9cee9385eb887ac3401c31aea
Sam <301109640@qq.com>
2021-05-27 12:03:36 +0800

    update to v0.6.5

--- lib/config/ini_test.go
+++ lib/config/ini_test.go
@@ -9,21 +9,19 @@ func TestINI_GetKey(t *testing.T) {
 
 	s := GetString("app_name")
 
-	s1 := GetString("gkzy-user::user")
+	fmt.Printf("s type=%T,s=%v \n", s, s)
 
-	i1, _ := GetInt("gkzy-user::port")
+	_, err := WriteContent("123456")
+	if err != nil {
+		fmt.Println("err:", err)
+	}
 
-	f1, _ := GetFloat("score")
 
-	b1, _ := GetBool("recover_panic")
+	Reload()
 
-	p1 := GetString("app_mode")
+	s = GetString("app_name")
 
 	fmt.Printf("s type=%T,s=%v \n", s, s)
-	fmt.Printf("s1 type=%T,s=%v \n", s1, s1)
-	fmt.Printf("i1 type=%T,i1=%v \n", i1, i1)
-	fmt.Printf("f1 type=%T,f1=%v \n", f1, f1)
-	fmt.Printf("b1, type=%T,b1=%v \n", b1, b1)
-	fmt.Printf("p1, type=%T,p1=%v \n", p1, p1)
+
 
 }
